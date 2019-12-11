e14eb3c10e79dfd509191fdf94bbb62546b092b3
Tim <tim.weinert@gmail.com>
2019-12-11 14:53:14 +1100

    Generator functions - first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// Generator functions --- http://www.net-comber.com/charset.html
+
+function getRandomLower(){
+return String.fromCharCode(Math.floor(Math.random() * 26) +97)
+
+}
+
+console.log(getRandomLower());
