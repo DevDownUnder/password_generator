8caeae16e50bc23ae2d26b1b5214a5e6eb7d06ec
Tim <tim.weinert@gmail.com>
2019-12-11 15:10:38 +1100

    Code - Lower and uppercase randomiser  + comments

--- app.js
+++ app.js
@@ -2,7 +2,22 @@
 
 function getRandomLower(){
 return String.fromCharCode(Math.floor(Math.random() * 26) +97)
-
+//return a string (letter) from the unicode number
+//Math.floor - round downto an interger (no decimals)
+//Math.random - select a random between 0 and 1 
+// 26 for the number of letters in the alphabet
+// 97 is where the lower case alphabet begins in unicode
 }
 
 console.log(getRandomLower());
+
+function getRandomUpper(){
+    return String.fromCharCode(Math.floor(Math.random() * 26) +65)
+    //return a string (letter) from the unicode number
+    //Math.floor - round downto an interger (no decimals)
+    //Math.random - select a random between 0 and 1 
+    // 26 for the number of letters in the alphabet
+    // 65 is where the lower case alphabet begins in unicode
+    }
+    
+    console.log(getRandomUpper());
